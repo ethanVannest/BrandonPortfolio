218c87c48211be594bfc2b2053a3a78fe6f741f4
ethanvannest <ethan3vannest@gmail.com>
2023-02-13 21:14:24 -0700

    page links and connect page

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+      <div className='contactHeaderDiv'>
+                <div className='left'></div>
+                    <h1 className='connectHeader'>Connect
+                    </h1>
+                <div className='right'></div>
+            </div>
+        <div className='contactDiv'>
+            <form>
+            
+            <input placeholder='Name' type="text" name="user_name" />
+            
+            <input placeholder='Email' type="email" name="user_email" />
+
+            <textarea name="message" placeholder='Please reach out with any ambitious projects, questions, ideas, or concerns!' />
+            <input type="submit" value="Send" />
+            </form>
+          </div>
+    </div>
   )
 }
 
--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div>
+      <img src='https://t4.ftcdn.net/jpg/04/83/17/69/360_F_483176994_Mvj1b9H2sKpq9T1xzoJy6CreJT7leHBL.jpg'></img>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li>Gallery</li>
+        <li> <Link to="contact">Contact</Link></li>
+        <li>About</li>
+      </ul>
+    </div>
   )
 }
 
