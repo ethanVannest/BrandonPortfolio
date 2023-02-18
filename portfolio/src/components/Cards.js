1fd65c71b433e29f09048e9ca7f01c432ebee233
ethanvannest <ethan3vannest@gmail.com>
2023-02-18 12:15:39 -0700

    added first image

--- portfolio/src/components/Cards.js
+++ portfolio/src/components/Cards.js
@@ -1,8 +1,14 @@
 import React from 'react'
 
+
 const Cards = () => {
   return (
-    <div>Cards</div>
+    <div>
+      <div className='productOne'>
+        <img src={require('../imgs/20170119_171628.jpg')} alt='project 1'/>
+        <p>Beautiful Tree</p>
+      </div>
+    </div>
   )
 }
 
--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom'
 const Header = () => {
   return (
     <div>
-      <img src='https://t4.ftcdn.net/jpg/04/83/17/69/360_F_483176994_Mvj1b9H2sKpq9T1xzoJy6CreJT7leHBL.jpg'></img>
+      <img src='https://t4.ftcdn.net/jpg/04/83/17/69/360_F_483176994_Mvj1b9H2sKpq9T1xzoJy6CreJT7leHBL.jpg' alt="B img"></img>
       <ul>
         <li><Link to="/">Home</Link></li>
         <li>Gallery</li>
-        <li> <Link to="contact">Contact</Link></li>
+        <li> <Link to="/contact">Contact</Link></li>
         <li>About</li>
       </ul>
     </div>
