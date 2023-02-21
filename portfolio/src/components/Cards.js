a953878a59ff35c67ed40489f0dc2ee469ed42ef
ethanvannest <ethan3vannest@gmail.com>
2023-02-20 22:08:44 -0700

    put images on page

--- portfolio/src/components/Cards.js
+++ portfolio/src/components/Cards.js
@@ -7,7 +7,22 @@ const Cards = () => {
       <div className='productOne'>
         <img src={require('../imgs/20170119_171628.jpg')} alt='project 1' className='max-w-sm'/>
         <img src={require('../imgs/20180927_091420.jpg')} alt='project 1' className='max-w-sm'/>
-        <img src={require('../imgs/20180927_091420.jpg')} alt='project 1' className='max-w-sm'/>
+        <img src={require('../imgs/20190322_135523.jpg')} alt='project 1' className='max-w-sm'/>
+        <img src={require('../imgs/20170815_121140.jpg')} alt='project 1' className='max-w-sm'/>
+        <img src={require('../imgs/FB_IMG_1487639317339.jpg')} alt='project 1' className='max-w-sm'/>
+        <img src={require('../imgs/Screenshot_20200129-063832_Facebook_2.jpg')} alt='project 1' className='max-w-sm'/>
+        <img src={require('../imgs/IMG_20180428_113807627.jpg')} alt='project 1' className='max-w-sm'/>
+        <img src={require('../imgs/IMG_20180419_165112457_HDR.jpg')} alt='project 1' className='max-w-sm'/>
+        <img src={require('../imgs/IMG_20180404_134212526_HDR.jpg')} alt='project 1' className='max-w-sm'/>
+        <img src={require('../imgs/DSC_0063.jpg')} alt='project 1' className='max-w-sm'/>
+        <img src={require('../imgs/DSC_0041.jpg')} alt='project 1' className='max-w-sm'/>
+        <img src={require('../imgs/DSC_0011.jpg')} alt='project 1' className='max-w-sm'/>
+        <img src={require('../imgs/20200706_191529.jpg')} alt='project 1' className='max-w-sm'/>
+        <img src={require('../imgs/20200623_132448.jpg')} alt='project 1' className='max-w-sm'/>
+        <img src={require('../imgs/20200602_120045.jpg')} alt='project 1' className='max-w-sm'/>
+        <img src={require('../imgs/20200308_184913.jpg')} alt='project 1' className='max-w-sm'/>
+        <img src={require('../imgs/20190609_185614.jpg')} alt='project 1' className='max-w-sm'/>
+        <img src={require('../imgs/20181102_121102.jpg')} alt='project 1' className='max-w-sm'/>
       </div>
     </div>
   )
