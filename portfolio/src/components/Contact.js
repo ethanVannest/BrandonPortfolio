138abdfadeddac044496ace2e27ce96d2c08990e
ethanvannest <ethan3vannest@gmail.com>
2023-02-24 12:05:11 -0700

    created contact page

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -3,23 +3,110 @@ import React from 'react'
 const Contact = () => {
   return (
     <div>
-      <div className='contactHeaderDiv'>
-                <div className='left'></div>
-                    <h1 className='connectHeader'>Connect
-                    </h1>
-                <div className='right'></div>
-            </div>
-        <div className='contactDiv'>
-            <form>
-            
-            <input placeholder='Name' type="text" name="user_name" />
-            
-            <input placeholder='Email' type="email" name="user_email" />
+      <section className="bg-white dark:bg-gray-900">
+        <div className="py-8 mt-14 lg:py-16 px-4 mx-auto max-w-screen-md">
+            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Me</h2>
+            <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Need a design? Want A custom metal artwork to wow friends and family? Need details? Let me know.</p>
+            <form className="space-y-8">
+                <div>
+                    <label for="Email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
+                    <input type="text" name="user_email" 
+                    placeholder="name@mail.com" 
+                    className="
+                    shadow-sm 
+                    bg-gray-50 border 
+                    border-gray-300 
+                    text-gray-900 
+                    text-sm 
+                    rounded-lg 
+                    focus:ring-primary-500 
+                    focus:border-primary-500 
+                    block w-full p-2.5 
+                    dark:bg-gray-700 
+                    dark:border-gray-600 
+                    dark:placeholder-gray-400 
+                    dark:text-white 
+                    dark:focus:ring-primary-500 
+                    dark:focus:border-primary-500 
+                    dark:shadow-sm-light"
+                    required/>
+                    
+                    
+                </div>
+                <div>
+                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Name</label>
+                    <input type="text" placeholder="Name" name="user_name"
+                    required
+                    className="
+                    block 
+                    p-3 
+                    w-full 
+                    text-sm 
+                    text-gray-900 
+                    bg-gray-50 
+                    rounded-lg 
+                    border 
+                    border-gray-300 
+                    shadow-sm 
+                    focus:ring-primary-500 
+                    focus:border-primary-500 
+                    dark:bg-gray-700 
+                    dark:border-gray-600 
+                    dark:placeholder-gray-400 
+                    dark:text-white 
+                    dark:focus:ring-primary-500 
+                    dark:focus:border-primary-500 
+                    dark:shadow-sm-light" 
+                    />
+                </div>
+                <div className="sm:col-span-2">
+                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
+                    <textarea name="messsage"rows="6" 
+                    placeholder="Leave a comment..."
+                    className="block 
+                    p-2.5 
+                    w-full 
+                    text-sm 
+                    text-gray-900 
+                    bg-gray-50 
+                    rounded-lg 
+                    shadow-sm 
+                    border 
+                    border-gray-300 
+                    focus:ring-primary-500 
+                    focus:border-primary-500 
+                    dark:bg-gray-700 
+                    dark:border-gray-600 
+                    dark:placeholder-gray-400 
+                    dark:text-white 
+                    dark:focus:ring-primary-500 
+                    dark:focus:border-primary-500">
 
-            <textarea name="message" placeholder='Please reach out with any ambitious projects, questions, ideas, or concerns!' />
-            <input type="submit" value="Send" />
+                    </textarea>
+                </div>
+                <button type="submit" value="Send"
+                className="py-3 
+                px-5 
+                text-sm 
+                font-medium 
+                text-center 
+                text-white 
+                rounded-lg 
+                bg-primary-700 
+                sm:w-fit 
+                hover:bg-primary-800 
+                focus:ring-4 
+                focus:outline-none 
+                focus:ring-primary-300 
+                dark:bg-primary-600 
+                dark:hover:bg-primary-700 
+                dark:focus:ring-primary-800 
+                hover:text-green-400 
+                border"
+                >Send message</button>
             </form>
-          </div>
+        </div>
+      </section>
     </div>
   )
 }
