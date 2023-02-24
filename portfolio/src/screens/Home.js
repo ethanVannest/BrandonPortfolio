356ec31694bb275fba29845e873aeb81ec4ba04f
ethanvannest <ethan3vannest@gmail.com>
2023-02-24 12:07:08 -0700

    added margin to home

--- portfolio/src/screens/Home.js
+++ portfolio/src/screens/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Carousel from '../components/Carousel'
 const Home = () => {
   return (
-    <div className="2xl:container 2xl:mx-auto 2xl:px-0 py-3 px-10">
+    <div className="2xl:container mt-10 2xl:mx-auto 2xl:px-0 py-3 px-10">
         <Carousel />
     </div>
   )
