d6a728eafb13053106c78d63cf8806d83d22d809
ethanvannest <ethan3vannest@gmail.com>
2023-03-16 18:59:46 -0600

    db connection file

--- backend/config/db.connection.js
+++ backend/config/db.connection.js
@@ -1,7 +1,18 @@
+require("dotenv").config()
 const mongoose = require("mongoose")
 
 const connectionStr = async () => {
     try {
-        await mongoose.connect()
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+
+        console.log("DB connection SUCCESS")
+    } catch (error) {
+        console.log("DB connection Error") 
+        process.exit(1)
     }
 }
+
+module.exports = connectionStr
